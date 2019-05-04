b4febee8f7c63cf8b7c6e9ef6b9b3b51aa29d933
Fitzy <jfitzy1321@protonmail.com>
2019-05-04 12:34:40 -0600

    things todo after fetching repo

---
+++ instructions.py
@@ -0,0 +1,17 @@
+# install "Sweet" Kde theme and icons
+# install latest python version
+# install latest rust version
+# install git and gitkraken
+# install latest dotnet
+# install build-esstenials
+# install brave browser
+# install realvnc-vnc-viewer 
+# install visual studio code
+# import settings
+# get extensions
+# download repos
+# install nmap
+# connect to pi
+# use ifconfig to get local host ip
+# use nmap [ip from ifconfig].0-255
+# this should get all devices in that range of ip address
